bae5706491e4a0388bfd94fe7763d41c198baca5
yexu1229 <joezhouyx@hotmail.com>
2021-07-28 12:09:22 +0200

    t

--- train.py
+++ train.py
@@ -340,11 +340,11 @@ def train(hyp,  # path/to/hyp.yaml or hyp dictionary
             # Forward
             with amp.autocast(enabled=cuda):
                 pred = model(imgs)  # forward
-                print(".................")
-                print("targets: ",targets.size())
-                print("targets: ",targets)
-                print(".................")
-                print("imgs: ",imgs.size())
+                #print(".................")
+                #print("targets: ",targets.size())
+                #print("targets: ",targets)
+                #print(".................")
+                #print("imgs: ",imgs.size())
                 loss, loss_items = compute_loss(pred, targets.to(device))  # loss scaled by batch_size
                 if RANK != -1:
                     loss *= WORLD_SIZE  # gradient averaged between devices in DDP mode
